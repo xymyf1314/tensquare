33069fa8b0ce470832a37298dcdc37054c0c08b0
miyf <xymyf6@163.com>
2020-10-10 16:30:19 +0800

    recruit完成，开始Qa

--- tensquare_recruit/src/main/java/com/tensquare/recruit/dao/EnterpriseDao.java
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/dao/EnterpriseDao.java
@@ -4,11 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.recruit.pojo.Enterprise;
+
+import java.util.List;
+
 /**
  * 数据访问接口
- * @author Administrator
  *
+ * @author miyf
  */
-public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
-	
+public interface EnterpriseDao extends JpaRepository<Enterprise, String>, JpaSpecificationExecutor<Enterprise> {
+    public List<Enterprise> findByIshot(String ishot);// where istot = ?
 }
--- tensquare_recruit/src/main/java/com/tensquare/recruit/dao/RecruitDao.java
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/dao/RecruitDao.java
@@ -4,11 +4,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.recruit.pojo.Recruit;
+
+import java.util.List;
+
 /**
  * 数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface RecruitDao extends JpaRepository<Recruit,String>,JpaSpecificationExecutor<Recruit>{
-	
+public interface RecruitDao extends JpaRepository<Recruit, String>, JpaSpecificationExecutor<Recruit> {
+    /**
+     * 推荐职位
+     * 日期: 16:13 2020/10/10
+     * 作者: miyf
+     *
+     * @param state 职位状态 0 关闭 1开启 2推荐
+     * @return List<Recruit>
+     **/
+    public List<Recruit> findTop6ByStateOrderByCreatetimeDesc(String state); // where state = ? order by createtime
+
+    /**
+     * 最新职位
+     * 日期: 16:13 2020/10/10
+     * 作者: miyf
+     *
+     * @param state 职位状态 0 关闭 1开启 2推荐
+     * @return List<Recruit>
+     **/
+    public List<Recruit> findTop6ByStateNotOrderByCreatetimeDesc(String state); // where state != ? order by createtime
+
 }
