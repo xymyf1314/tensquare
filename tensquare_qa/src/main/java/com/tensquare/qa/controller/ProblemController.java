f1aa0c14602277006dbc66f2d1023e37b4fd9ae8
miyf <xymyf6@163.com>
2020-10-12 17:47:57 +0800

    Qa完成

--- tensquare_qa/src/main/java/com/tensquare/qa/controller/ProblemController.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/controller/ProblemController.java
@@ -1,108 +1,156 @@
 package com.tensquare.qa.controller;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.tensquare.qa.pojo.Problem;
 import com.tensquare.qa.service.ProblemService;
-
 import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 /**
  * 控制器层
- * @author Administrator
  *
+ * @author Administrator
  */
 @RestController
 @CrossOrigin
 @RequestMapping("/problem")
 public class ProblemController {
 
-	@Autowired
-	private ProblemService problemService;
-	
-	
-	/**
-	 * 查询全部数据
-	 * @return
-	 */
-	@RequestMapping(method= RequestMethod.GET)
-	public Result findAll(){
-		return new Result(true,StatusCode.OK,"查询成功",problemService.findAll());
-	}
-	
-	/**
-	 * 根据ID查询
-	 * @param id ID
-	 * @return
-	 */
-	@RequestMapping(value="/{id}",method= RequestMethod.GET)
-	public Result findById(@PathVariable String id){
-		return new Result(true,StatusCode.OK,"查询成功",problemService.findById(id));
-	}
+    @Autowired
+    private ProblemService problemService;
+
+    /**
+     * 最新回答列表
+     * 日期: 17:38 2020/10/10
+     * 作者: miyf
+     *
+     * @param labelId 标签id
+     * @param page    页码
+     * @param size    页大小
+     * @return Result
+     **/
+    @RequestMapping(value = "/newlist/{labelId}/{page}/{size}", method = RequestMethod.GET)
+    public Result newList(@PathVariable String labelId, @PathVariable int page, @PathVariable int size) {
+        Page<Problem> problems = problemService.newList(labelId, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Problem>(problems.getTotalElements(), problems.getContent()));
+    }
+
+    /**
+     * 热门回答列表
+     * 日期: 17:38 2020/10/10
+     * 作者: miyf
+     *
+     * @param labelId 标签id
+     * @param page    页码
+     * @param size    页大小
+     * @return Result
+     **/
+    @RequestMapping(value = "/hotlist/{labelId}/{page}/{size}", method = RequestMethod.GET)
+    public Result hotList(@PathVariable String labelId, @PathVariable int page, @PathVariable int size) {
+        Page<Problem> problems = problemService.hotList(labelId, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Problem>(problems.getTotalElements(), problems.getContent()));
+    }
+
+    /**
+     * 等待回答列表
+     * 日期: 17:39 2020/10/10
+     * 作者: miyf
+     *
+     * @param labelId 标签id
+     * @param page    页码
+     * @param size    页大小
+     * @return Result
+     **/
+    @RequestMapping(value = "/waitlist/{labelId}/{page}/{size}", method = RequestMethod.GET)
+    public Result waitList(@PathVariable String labelId, @PathVariable int page, @PathVariable int size) {
+        Page<Problem> problems = problemService.waitList(labelId, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Problem>(problems.getTotalElements(), problems.getContent()));
+    }
+
+    /**
+     * 查询全部数据
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功", problemService.findAll());
+    }
+
+    /**
+     * 根据ID查询
+     *
+     * @param id ID
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true, StatusCode.OK, "查询成功", problemService.findById(id));
+    }
 
 
-	/**
-	 * 分页+多条件查询
-	 * @param searchMap 查询条件封装
-	 * @param page 页码
-	 * @param size 页大小
-	 * @return 分页结果
-	 */
-	@RequestMapping(value="/search/{page}/{size}",method=RequestMethod.POST)
-	public Result findSearch(@RequestBody Map searchMap , @PathVariable int page, @PathVariable int size){
-		Page<Problem> pageList = problemService.findSearch(searchMap, page, size);
-		return  new Result(true,StatusCode.OK,"查询成功",  new PageResult<Problem>(pageList.getTotalElements(), pageList.getContent()) );
-	}
+    /**
+     * 分页+多条件查询
+     *
+     * @param searchMap 查询条件封装
+     * @param page      页码
+     * @param size      页大小
+     * @return 分页结果
+     */
+    @RequestMapping(value = "/search/{page}/{size}", method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap, @PathVariable int page, @PathVariable int size) {
+        Page<Problem> pageList = problemService.findSearch(searchMap, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Problem>(pageList.getTotalElements(), pageList.getContent()));
+    }
 
-	/**
+    /**
      * 根据条件查询
+     *
      * @param searchMap
      * @return
      */
-    @RequestMapping(value="/search",method = RequestMethod.POST)
-    public Result findSearch( @RequestBody Map searchMap){
-        return new Result(true,StatusCode.OK,"查询成功",problemService.findSearch(searchMap));
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap) {
+        return new Result(true, StatusCode.OK, "查询成功", problemService.findSearch(searchMap));
     }
-	
-	/**
-	 * 增加
-	 * @param problem
-	 */
-	@RequestMapping(method=RequestMethod.POST)
-	public Result add(@RequestBody Problem problem  ){
-		problemService.add(problem);
-		return new Result(true,StatusCode.OK,"增加成功");
-	}
-	
-	/**
-	 * 修改
-	 * @param problem
-	 */
-	@RequestMapping(value="/{id}",method= RequestMethod.PUT)
-	public Result update(@RequestBody Problem problem, @PathVariable String id ){
-		problem.setId(id);
-		problemService.update(problem);		
-		return new Result(true,StatusCode.OK,"修改成功");
-	}
-	
-	/**
-	 * 删除
-	 * @param id
-	 */
-	@RequestMapping(value="/{id}",method= RequestMethod.DELETE)
-	public Result delete(@PathVariable String id ){
-		problemService.deleteById(id);
-		return new Result(true,StatusCode.OK,"删除成功");
-	}
-	
+
+    /**
+     * 增加
+     *
+     * @param problem
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Problem problem) {
+        problemService.add(problem);
+        return new Result(true, StatusCode.OK, "增加成功");
+    }
+
+    /**
+     * 修改
+     *
+     * @param problem
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Problem problem, @PathVariable String id) {
+        problem.setId(id);
+        problemService.update(problem);
+        return new Result(true, StatusCode.OK, "修改成功");
+    }
+
+    /**
+     * 删除
+     *
+     * @param id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result delete(@PathVariable String id) {
+        problemService.deleteById(id);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+
 }
--- tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -1,14 +1,51 @@
 package com.tensquare.qa.dao;
 
+import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-import com.tensquare.qa.pojo.Problem;
 /**
  * 数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+public interface ProblemDao extends JpaRepository<Problem, String>, JpaSpecificationExecutor<Problem> {
+    /**
+     * 最新回答
+     * 日期: 17:25 2020/10/10
+     * 作者: miyf
+     *
+     * @param labelid  标签
+     * @param pageable 分页对象
+     * @return List<Problem>
+     **/
+    @Query(value = "SELECT * FROM  tb_problem tp,tb_pl tpl WHERE tp.id = tpl.problemid AND labelid = ?  ORDER BY tp.replytime DESC", nativeQuery = true)
+    public Page<Problem> newList(String labelid, Pageable pageable);
+
+    /**
+     * 最热回答
+     * 日期: 17:25 2020/10/10
+     * 作者: miyf
+     *
+     * @param labelid  标签
+     * @param pageable 分页对象
+     * @return List<Problem>
+     **/
+    @Query(value = "SELECT * FROM  tb_problem tp,tb_pl tpl WHERE tp.id = tpl.problemid AND labelid = ?  ORDER BY tp.reply DESC", nativeQuery = true)
+    public Page<Problem> hotList(String labelid, Pageable pageable);
+
+    /*
+     * 等待回答
+     * 日期: 17:26 2020/10/10
+     * 作者: miyf
+     * @param labelid 标签
+     * @param pageable 分页对象
+     * @return List<Problem>
+     **/
+    @Query(value = "SELECT * FROM  tb_problem tp,tb_pl tpl WHERE tp.id = tpl.problemid AND labelid = ? AND reply=0 ORDER BY tp.createtime DESC", nativeQuery = true)
+    public Page<Problem> waitList(String labelid, Pageable pageable);
+
 }
