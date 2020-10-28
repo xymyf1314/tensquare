469574ae5736e8d89d239ef3bdf459e690a86bdb
miyf <xymyf6@163.com>
2020-10-28 10:00:56 +0800

    article完成，开始gathering

--- tensquare_gathering/src/main/java/com/tensquare/gathering/controller/BaseExceptionHandler.java
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/controller/BaseExceptionHandler.java
@@ -4,9 +4,6 @@ import entity.StatusCode;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 /**
  * 统一异常处理类
  */
--- tensquare_gathering/src/main/java/com/tensquare/gathering/dao/GatheringDao.java
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/dao/GatheringDao.java
@@ -1,9 +1,8 @@
 package com.tensquare.gathering.dao;
 
+import com.tensquare.gathering.pojo.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import com.tensquare.gathering.pojo.Gathering;
 /**
  * 数据访问接口
  * @author Administrator
